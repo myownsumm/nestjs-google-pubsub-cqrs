062ef66f1f49b9483bd88c5c8cdecdd486630cfe
myownsumm <murmur_spree_0m@icloud.com>
2025-07-13 17:05:09 +0200

    Install pub-sub event bus lib.

--- integration-testing/monorepo/apps/auth-service/src/auth-service.module.ts
+++ integration-testing/monorepo/apps/auth-service/src/auth-service.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
+import { PubSubCqrsModule } from 'nestjs-google-pubsub-cqrs';
 import { AuthServiceController } from './auth-service.controller';
 import { AuthServiceService } from './auth-service.service';
 
 @Module({
-  imports: [],
+  imports: [
+    PubSubCqrsModule.forRoot({
+      subscriptionName: 'auth-service-sub',
+      topicName: 'integration-events-topic',
+      projectId: 'integration-test-project',
+      apiEndpoint: 'localhost', // for local emulator
+      port: 8085, // for local emulator
+    }),
+  ],
   controllers: [AuthServiceController],
   providers: [AuthServiceService],
 })
--- integration-testing/monorepo/apps/users-service/src/users-service.module.ts
+++ integration-testing/monorepo/apps/users-service/src/users-service.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
+import { PubSubCqrsModule } from 'nestjs-google-pubsub-cqrs';
 import { UsersServiceController } from './users-service.controller';
 import { UsersServiceService } from './users-service.service';
 
 @Module({
-  imports: [],
+  imports: [
+    PubSubCqrsModule.forRoot({
+      subscriptionName: 'users-service-sub',
+      topicName: 'integration-events-topic',
+      projectId: 'integration-test-project',
+      apiEndpoint: 'localhost', // for local emulator
+      port: 8085, // for local emulator
+    }),
+  ],
   controllers: [UsersServiceController],
   providers: [UsersServiceService],
 })
