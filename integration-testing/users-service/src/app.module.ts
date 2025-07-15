d70091f71a98d359a1a2dc7ad73f66649c34af19
myownsumm <murmur_spree_0m@icloud.com>
2025-07-15 13:08:07 +0200

    Install additional App with nest v11

---
+++ integration-testing/users-service/src/app.module.ts
@@ -0,0 +1,19 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { PubSubCqrsModule } from 'nestjs-google-pubsub-cqrs';
+
+@Module({
+  imports: [
+    PubSubCqrsModule.forRoot({
+      subscriptionName: 'users-service-sub',
+      topicName: 'users-topic',
+      projectId: 'your-gcp-project-id',
+      apiEndpoint: 'localhost', // optional, for local emulator
+      port: 8085 // optional, for local emulator
+    })
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
