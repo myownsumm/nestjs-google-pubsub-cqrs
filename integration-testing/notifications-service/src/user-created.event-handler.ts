43dbf0b223471aad1b8b871451a4602deb873d2b
myownsumm <murmur_spree_0m@icloud.com>
2025-07-17 14:27:03 +0200

    Log artifacts when after handler run.

--- integration-testing/notifications-service/src/user-created.event-handler.ts
+++ integration-testing/notifications-service/src/user-created.event-handler.ts
@@ -1,12 +1,14 @@
 import { EventsHandler, IEventHandler } from '@nestjs/cqrs';
 import { UserCreatedEvent } from './user-created.event';
+import { logArtifact } from '../../shared/artifact-logger';
 
 @EventsHandler(UserCreatedEvent)
 export class UserCreatedEventHandler
   implements IEventHandler<UserCreatedEvent>
 {
   async handle(event: UserCreatedEvent) {
-    console.log(
+    await logArtifact(
+      'notifications-service',
       `UserCreatedEvent received: ${event.payload.userId}, ${event.payload.email}`,
     );
   }
