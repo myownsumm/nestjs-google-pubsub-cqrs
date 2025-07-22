503d089e68a3b5438b35e4791ab0b49f3b46878d
myownsumm <murmur_spree_0m@icloud.com>
2025-07-22 23:20:02 +0200

    Extend timer.

--- integration-testing/ci/run-integration-tests.js
+++ integration-testing/ci/run-integration-tests.js
@@ -21,7 +21,7 @@ for (const script of testScripts) {
   try {
     execSync(`bash ${path.join(ciDir, script)}`, { stdio: 'inherit' });
     // Wait for events to be processed and written to logs
-    execSync('sleep 2');
+    execSync('sleep 5');
   } catch (e) {
     console.error(`❌ Script ${script} failed to execute`);
     allPassed = false;
